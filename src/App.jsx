62a3cc3c0f80701a5b11ca35066abc572cbb6c9c
FatAhFrog <mandoanddoug@gmail.com>
2025-12-21 11:08:42 -0600

    Implement auto-scroll for outputRef on tab change
    Add useEffect to scroll to the bottom of outputRef when tabs change.

--- src/App.jsx
+++ src/App.jsx
@@ -48,9 +48,13 @@ function App() {
     }
   };
 
+  useEffect(() => {
+    if (outputRef.current) outputRef.current.scrollTop = outputRef.current.scrollHeight;
+  }, [tabs]);
+
   window.addEventListener("keydown", handleKey);
   return () => window.removeEventListener("keydown", handleKey);
-}, [tabs]);
+  }, [tabs]);
 
 
   // Refresh only the current tab
